2847d38d213973d7c5be106d9d08ae451944c501
rootlamine <bthiam0301@gmail.com>
2025-10-26 20:06:27 +0000

    Creation des fichiers pour chaque entite

---
+++ app/Http/Controllers/UtilisateurController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUtilisateurRequest;
+use App\Http\Requests\UpdateUtilisateurRequest;
+use App\Models\Utilisateur;
+
+class UtilisateurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUtilisateurRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Utilisateur $utilisateur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUtilisateurRequest $request, Utilisateur $utilisateur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Utilisateur $utilisateur)
+    {
+        //
+    }
+}
