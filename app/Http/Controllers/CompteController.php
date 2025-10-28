25d62d425e4c3e80404ed6212a8c8bc7e87af536
rootlamine <bthiam0301@gmail.com>
2025-10-28 15:27:57 +0000

    modification methode

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -13,7 +13,7 @@ class CompteController extends Controller
      */
     public function index()
     {
-        return Compte::paginate();
+        return Compte::all();
     }
 
     /**
@@ -29,7 +29,7 @@ class CompteController extends Controller
      */
     public function show(Compte $compte)
     {
-        
+
     }
 
     /**
